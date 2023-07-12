132268b9c0293f1729369156a18beb9dedd28c37
GerryMcSam <130856341+GerryMcSam@users.noreply.github.com>
2023-07-11 23:14:06 -0400

    Create name.py

---
+++ name.py
@@ -0,0 +1,12 @@
+import random
+
+def generate_fake_name():
+  """Generates a fake name."""
+  first_name_list = ["John", "Jane", "Mary", "Peter", "Paul", "David"]
+  last_name_list = ["Smith", "Jones", "Williams", "Brown", "Davis", "Wilson"]
+  first_name = random.choice(first_name_list)
+  last_name = random.choice(last_name_list)
+  return first_name + " " + last_name
+
+if __name__ == "__main__":
+  print(generate_fake_name())
