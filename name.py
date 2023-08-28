e42cb4935234005f8987975f2c577635091915f5
GerryMcSam <130856341+GerryMcSam@users.noreply.github.com>
2023-08-28 13:01:44 -0400

    Update name.py

--- name.py
+++ name.py
@@ -2,7 +2,7 @@ import random
 
 def generate_fake_name():
   """Generates a fake name."""
-  first_name_list = ["John", "Jane", "Mary", "Peter", "Paul", "David"]
+  first_name_list = ["John", "Jane", "Mary", "Peter", "Paul", "David","Jonas"]
   last_name_list = ["Smith", "Jones", "Williams", "Brown", "Davis", "Wilson"]
   first_name = random.choice(first_name_list)
   last_name = random.choice(last_name_list)
