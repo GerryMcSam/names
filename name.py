5eb63b03c369bc5fcb69dcb7fcd6f7995ebbe2ab
GerryMcSam <130856341+GerryMcSam@users.noreply.github.com>
2023-09-01 08:09:32 -0400

    Update name.py
    adding 2

--- name.py
+++ name.py
@@ -2,8 +2,8 @@ import random
 
 def generate_fake_name():
   """Generates a fake name."""
-  first_name_list = ["John", "Jane", "Mary", "Peter", "Paul", "David","Jonas"]
-  last_name_list = ["Smith", "Jones", "Williams", "Brown", "Davis", "Wilson"]
+  first_name_list = ["John", "Jane", "Mary", "Peter", "Paul", "David","Jonas","Hannah"]
+  last_name_list = ["Smith", "Jones", "Williams", "Brown", "Davis", "Wilson","Wilpers"]
   first_name = random.choice(first_name_list)
   last_name = random.choice(last_name_list)
   return first_name + " " + last_name
